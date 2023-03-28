a2942bd9f5c6548091f4f3163da10a3c4d49678d
pdbautista0 <patdi.bautista@gmail.com>
2023-03-28 08:35:52 +0800

    Added Player Piece Selection Element

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import { fooHandler } from './src/evenHandler'
-import player from './src/player'
-import board from './src/board'
-import { checkForHorizontalWinningCombination } from './src/helpers/WinnerChecker'
-import { runTest, runTest2, runTest3 } from './src/helpers/test'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import player from "./src/player";
+import board from "./src/board";
 
-
-const player1 = player("Human", 'x')
-const player2 = player("Alien", 'o')
+const player1 = player("Player1", "x");
+const player2 = player("Alien", "o");
 const gameBoard = board();
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>Tik Tac Toe</h1>
+    <div id="player-selection">
+    <div>
+    <label for="player1-piece">${player1.name}</label>
+    <button id="select"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div>
+    <label for="player">${player2.name}</label>
+    <button id="select"></button>
+    </div>
+        
+        
+      </div>
+    <div class="content">
+    </div>
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
+gameBoard.createTheBoard(document.querySelector(".content"));
+// setupCounter(document.querySelector('#counter'))
