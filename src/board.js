f517977e38567aca1bdb12749035dd803b7dc795
pdbautista0 <patdi.bautista@gmail.com>
2023-03-28 08:34:20 +0800

    Added Pieces

--- src/board.js
+++ src/board.js
@@ -3,20 +3,67 @@ import {
   checkForHorizontalWinningCombination,
   checkForVerticalWinningCombination,
 } from "./helpers/WinnerChecker";
+import close from "./assets/close.svg";
+import circle from "./assets/circle.svg";
 
 function board() {
   const board = [
     ["", "", ""],
-    ["s", "s", "s"],
-    ["s", "s", "s"],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  const pieces = [
+    {
+      type: "x",
+      element: () => {
+        return `<img src=${close} alt="close" />`;
+      },
+    },
+    {
+      type:'o',
+      element:() => {
+        return `<img src=${circle} alt="circle" />`
+      }
+    }
   ];
   let endOfGame = false;
-  const makeAMove = (x, y, type) => {
-    board[y - 1][x - 1] = type;
+  let isBoardCreated = false;
+  let isGameStarted = false;
+
+  const makeAMove = (col, row, type) => {
+    board[row - 1][col - 1] = type;
   };
+
   const getBoard = () => {
     return board;
   };
+
+  const createTheBoard = (element) => {
+    if (!isBoardCreated) {
+      element.innerHTML = `
+    <div class="board">
+      <div class="cell-item" id="11"></div>
+      <div class="cell-item" id="12"></div>
+      <div class="cell-item" id="13"></div>
+      <div class="cell-item" id="21"></div>
+      <div class="cell-item" id="22"></div>
+      <div class="cell-item" id="23"></div>
+      <div class="cell-item" id="31"></div>
+      <div class="cell-item" id="32"></div>
+      <div class="cell-item" id="33"></div>
+    </div>
+    `;
+      isBoardCreated = true;
+    }
+  };
+
+  const gameStart = (element) => {
+    element.addEventListener('click', () => {
+      isGameStarted = true
+      element
+    })
+  }
+
   const checkForWinner = (player1, player2) => {
     if (
       checkForVerticalWinningCombination(board, player1, 0) &&
@@ -32,7 +79,7 @@ function board() {
       console.log("Winner Player 2");
     }
   };
-  return { makeAMove, getBoard };
+  return { makeAMove, getBoard, checkForWinner, createTheBoard };
 }
 
 export default board;
