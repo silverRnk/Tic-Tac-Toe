8afe77b11956a855e571ba80b2d6249194a383ff
pdbautista0 <patdi.bautista@gmail.com>
2023-04-01 01:39:02 +0800

    Edit Test
    change {name: '', type:''} to player(name, {type:'', element:''}

--- test/diagonalcheck.test.jsx
+++ test/diagonalcheck.test.jsx
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 import { checkForDiagonalWinningCondition } from "../src/helpers/WinnerChecker";
-
+import player from "../src/player";
 const testCase3 = [[], [], []]
 const testCase4 = []
 const testCase5 = [['x', 'x', 'x'], ['s', 'x', 's'], ['z', 'z', 'x']]
@@ -8,18 +8,19 @@ const testCase6 = [['x', 'x', 'z'], ['s', 'z', 's'], ['z', 's', 'x']]
 
 
 test('If Empty Array', () => { 
-    expect(checkForDiagonalWinningCondition(testCase3, {name: 'foo', type:'x'})).toBe(false)
-    expect(checkForDiagonalWinningCondition(testCase4, {name: 'foo', type:'x'})).toBe(false)
+    expect(checkForDiagonalWinningCondition(testCase3, player('foo', {type: 'x', piece: ''}))).toBe(false)
+    expect(checkForDiagonalWinningCondition(testCase4, player('foo', {type: 'x', piece: ''}))).toBe(false)
  })
 
  test('If No Winning Combination', () => {
-    expect(checkForDiagonalWinningCondition(testCase5, {name: 'foo', type:'s'})).toBe(false)
+    expect(checkForDiagonalWinningCondition(testCase5, player('foo', {type: 's', piece: ''}))).toBe(false)
  })
 
  test('If Winning Combination is At 1-5-9 Diagonal Combination', () => {
-    expect(checkForDiagonalWinningCondition(testCase5, {name: 'foo', type:'x'})).toBe(true)
+    expect(checkForDiagonalWinningCondition(testCase5, player('foo', {type: 'x', piece: ''}))).toBe(true)
  })
 
  test('If Winning Combination is At 1-5-9 Diagonal Combination', () => {
-    expect(checkForDiagonalWinningCondition(testCase6, {name: 'foo', type:'z'})).toBe(true)
+    expect(checkForDiagonalWinningCondition(testCase6, player('foo', 
+    {type: 'z', piece: ''}))).toBe(true)
  })
--- test/helper.test.jsx
+++ test/helper.test.jsx
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
 import { checkForVerticalWinningCombination, checkForHorizontalWinningCombination } from "../src/helpers/WinnerChecker";
+import player from "../src/player";
 
 const testCase1 = [['x', 'x', 'x'], ['s', 's', 's'], ['z', 'z', 'z']]
 const testCase2 = [['x', 'y', 'z'], ['x', 'y', 'z'], ['x', 'y', 'z']]
@@ -8,46 +9,46 @@ const testCase4 = []
 
 
 test('If empty array', () => {
-    expect(checkForHorizontalWinningCombination(testCase4, {name: 'foo', type:'f'})).toBe(false)
+    expect(checkForHorizontalWinningCombination(testCase4, player('foo', {type: 's', piece: ''}))).toBe(false)
 })
 
 test('If there is no winning combination', () => {
     
-    expect(checkForHorizontalWinningCombination(testCase1, {name: 'foo', type:'f'})).toBe(false)
+    expect(checkForHorizontalWinningCombination(testCase1, player('foo', {type: 'f', piece: ''}))).toBe(false)
 })
 
 test('If winning combination is a first row', () => {
     
-    expect(checkForHorizontalWinningCombination(testCase1, {name: 'foo', type: 'x'})).toBe(true)
+    expect(checkForHorizontalWinningCombination(testCase1, player('foo', {type: 'x', piece: ''}))).toBe(true)
 })
 
 test('If winning combination is a second row', () => {
     
-    expect(checkForHorizontalWinningCombination(testCase1, {name: 'foo', type: 's'})).toBe(true)
+    expect(checkForHorizontalWinningCombination(testCase1, player('foo', {type: 's', piece: ''}))).toBe(true)
 })
 
 test('If winning combination is a Third row', () => {
     
-    expect(checkForHorizontalWinningCombination(testCase1, {name: 'foo', type: 'z'})).toBe(true)
+    expect(checkForHorizontalWinningCombination(testCase1, player('foo', {type: 'z', piece: ''}))).toBe(true)
 })
 
 test('If empty array', () => {
-    expect(checkForVerticalWinningCombination(testCase3, {name: 'foo', type: 'q'}, 0)).toBe(false)
+    expect(checkForVerticalWinningCombination(testCase3, player('foo', {type: 'q', piece: ''}), 0)).toBe(false)
 })
 
 test('If no vertical winning condition', () => {
-    expect(checkForVerticalWinningCombination(testCase2, {name: 'foo', type: 'q'}, 0)).toBe(false)
+    expect(checkForVerticalWinningCombination(testCase2, player('foo', {type: 'q', piece: ''}), 0)).toBe(false)
 })
 
 test('If vertical winning condition is at first column', () => {
-    expect(checkForVerticalWinningCombination(testCase2, {name: 'foo', type: 'x'}, 0)).toBe(true)
+    expect(checkForVerticalWinningCombination(testCase2, player('foo', {type: 'x', piece: ''}), 0)).toBe(true)
 })
 
 test('If vertical winning condition is at second column', () => {
-    expect(checkForVerticalWinningCombination(testCase2, {name: 'foo', type: 'y'}, 0)).toBe(true)
+    expect(checkForVerticalWinningCombination(testCase2, player('foo', {type: 'y', piece: ''}), 0)).toBe(true)
 })
 
 test('If vertical winning condition is at third column', () => {
-    expect(checkForVerticalWinningCombination(testCase2, {name: 'foo', type: 'z'}, 0)).toBe(true)
+    expect(checkForVerticalWinningCombination(testCase2, player('foo', {type: 'z', piece: ''}), 0)).toBe(true)
 })
 
