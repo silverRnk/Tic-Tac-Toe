15b290fa2acf575194a5f038707a0ff14e043143
pdbautista0 <patdi.bautista@gmail.com>
2023-03-31 13:37:10 +0800

    Declared CreateLayout

--- main.js
+++ main.js
@@ -4,30 +4,32 @@ import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 import player from "./src/player";
 import board from "./src/board";
-import close from "/close.svg";
-import circle from "/circle.svg";
+import { CreateLayout } from "./src/HTMLayoutFactory/createLayout";
 
+const htmlLayoutFactory = CreateLayout()
 const gameBoard = board();
 
 document.querySelector("#app").innerHTML = `
-
 <div id="set-player-card"></div>
-  <div id="board">
-    <h1>Tik Tac Toe</h1>
+<h1>Tik Tac Toe</h1>
     <div id="player-selection">
         <div>
-        <label for="player1-piece">player</label>
-        <div class="select"><img src=${close} alt="close" /></div>
+        <label for="player1-piece">player 1</label>
+        <div class="select"></div>
         </div>
         <div>
-        <label for="player1-piece">player</label>
-        <div class="select">y</div>
+        <label for="player1-piece">player 2</label>
+        <div class="select"></div>
         </div> 
           </div>
-    <div class="content">
-    </div>
-  </div>
+          <div class="content">
+          </div>
 `;
+
+htmlLayoutFactory.playerSelectionCard(document
+  .querySelector('#set-player-card'))
+htmlLayoutFactory.createTicTakToeBoard(document.
+  querySelector(".content"))
 
 gameBoard.getBoardDOM(document.querySelectorAll(".cell-item"));
 gameBoard.enBoardDOM();
